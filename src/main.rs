9360ee14de10d1af64d5ca6169feb812e44a1e3e
John T. Wodder II <git@varonathe.org>
2022-11-19 18:12:57 -0500

    Support TLS

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,46 @@ use clap::Parser;
 use futures::{SinkExt, StreamExt};
 use rustyline_async::{Readline, ReadlineError};
 use std::io::Write;
+use std::pin::Pin;
+use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::net::TcpStream;
 use tokio_util::codec::{Framed, LinesCodec};
 
 #[derive(Parser)]
 struct Arguments {
+    #[clap(long)]
+    tls: bool,
     host: String,
     port: u16,
 }
+
+trait AsyncReadWrite: AsyncRead + AsyncWrite {}
+impl<T> AsyncReadWrite for T where T: AsyncRead + AsyncWrite {}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Arguments::parse();
     println!("* Connecting ...");
-    let conn = TcpStream::connect((args.host, args.port))
+    let conn = TcpStream::connect((args.host.clone(), args.port))
         .await
         .context("Error connecting to server")?;
     println!(
         "* Connected to {}",
         conn.peer_addr().context("Error getting peer address")?
     );
+    let conn: Pin<Box<dyn AsyncReadWrite>> = if args.tls {
+        println!("* Initializing TLS ...");
+        let cx = tokio_native_tls::TlsConnector::from(
+            native_tls::TlsConnector::new().context("Error creating TLS connector")?,
+        );
+        Box::pin(
+            cx.connect(&args.host, conn)
+                .await
+                .context("Error establishing TLS connection")?,
+        )
+    } else {
+        Box::pin(conn)
+    };
     let mut frame = Framed::new(conn, LinesCodec::new_with_max_length(65535));
     let (mut rl, mut stdout) =
         Readline::new("netlion> ".into()).context("Error constructing Readline object")?;
