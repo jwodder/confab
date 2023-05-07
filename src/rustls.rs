1a119da468311e5dd00fac2f9fb1b3c34d87d1d3
John T. Wodder II <git@varonathe.org>
2023-05-07 14:08:44 -0400

    Replace `webpki-roots` (copyleft) with `rustls-native-certs`

--- src/rustls.rs
+++ src/rustls.rs
@@ -1,20 +1,22 @@
-use anyhow::Context;
+use anyhow::{bail, Context};
 use std::sync::Arc;
 use tokio::net::TcpStream;
-use tokio_rustls::rustls::{ClientConfig, OwnedTrustAnchor, RootCertStore, ServerName};
+use tokio_rustls::rustls::{ClientConfig, RootCertStore, ServerName};
 use tokio_rustls::TlsConnector;
 
 pub(crate) type TlsStream = tokio_rustls::client::TlsStream<TcpStream>;
 
 pub(crate) async fn connect(conn: TcpStream, servername: &str) -> anyhow::Result<TlsStream> {
     let mut root_cert_store = RootCertStore::empty();
-    root_cert_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.0.iter().map(|ta| {
-        OwnedTrustAnchor::from_subject_spki_name_constraints(
-            ta.subject,
-            ta.spki,
-            ta.name_constraints,
-        )
-    }));
+    let system_certs = rustls_native_certs::load_native_certs()
+        .context("Failed to load system certificate store")?
+        .into_iter()
+        .map(|cert| cert.0)
+        .collect::<Vec<_>>();
+    let (good, bad) = root_cert_store.add_parsable_certificates(&system_certs);
+    if good == 0 {
+        bail!("Failed to load any certificates from system store: all {bad} certs were invalid");
+    }
     let config = ClientConfig::builder()
         .with_safe_defaults()
         .with_root_certificates(root_cert_store)
